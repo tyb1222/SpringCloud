35a47c27e95d55bb0d929a53026a2eb9632b5d40
taoyongbo <153026583@163.com>
2019-03-30 17:30:55 +0800

    use random ribbon

--- microCloud-consumer-feign/src/main/java/com/tyb1222/consumer/feign/ConsumerFeignApp.java
+++ microCloud-consumer-feign/src/main/java/com/tyb1222/consumer/feign/ConsumerFeignApp.java
@@ -1,12 +1,15 @@
 package com.tyb1222.consumer.feign;
 
+import com.tyb1222.consumer.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "MICROCLOUD-PRODUCT-SERVER" ,configuration = RibbonConfig.class)
 @EnableFeignClients("com.tyb1222.fegin.service")
 public class ConsumerFeignApp {
     public static void main(String[] args) {
